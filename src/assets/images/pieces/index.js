8571fdd6c4edf3f260f4dad8a3581bc718ff5dbf
Malcolm <redwindmh@gmail.com>
2024-03-24 14:18:23 +0900

    Turned each game piece into individual asset

---
+++ src/assets/images/pieces/index.js
@@ -0,0 +1,8 @@
+export { default as storm } from "./storm.png"
+export { default as water } from "./water.png"
+export { default as fire } from "./fire.png"
+export { default as ice } from "./ice.png"
+export { default as wind } from "./wind.png"
+export { default as unknown } from "./unknown.png"
+export { default as tree_coin } from "./tree_coin.png"
+export { default as eye_coin } from "./eye_coin.png"
--- src/components/readyplayerone/readyplayerone.jsx
+++ src/components/readyplayerone/readyplayerone.jsx
@@ -1,10 +1,21 @@
-import pieces from "../../assets/images/pieces/covenants pieces.png"
+import { storm, water, fire, ice, wind, unknown, tree_coin, eye_coin } from "../../assets/images/pieces/index.js"
 
 const ReadyPlayerOne = () => {
   return (
-    <div className="w-1/3">
+    <div className="w-2/7">
       <h2 className="text-red-500">Player One</h2>
-      <img src={pieces} alt="pieces" />
+      <div className="grid grid-cols-3 gap-3">
+        <img src={storm} alt="pieces" />
+        <img src={water} alt="pieces" />
+        <img src={fire} alt="pieces" />
+        <img src={ice} alt="pieces" />
+        <img src={wind} alt="pieces" />
+        <img src={unknown} alt="pieces" />
+        <img src={fire} alt="pieces" />
+      </div>
+      <div className="grid gird-cols-4 gap-4">
+        <img src={tree_coin} alt="tree_coin" />
+      </div>
     </div>
   )
 }
