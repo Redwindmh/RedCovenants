883bb4da0f760a4c4bdb8f0e4bb966808e2f8434
Malcolm <redwindmh@gmail.com>
2024-04-07 12:55:02 +0900

    Testing, testing

--- src/components/mainboard/mainboard.jsx
+++ src/components/mainboard/mainboard.jsx
@@ -3,7 +3,7 @@ import mainboard from "../../assets//images/board/covenants_board2bg.png"
 const MainBoard = () => {
   return (
     <div className="w-3/7">
-      <h1 className="text-red-500">Covenants</h1>
+      <h1 className="text-red-500">Red Covenants</h1>
       <img src={mainboard} alt="board" className="h-auto max-w-full" />
     </div>
   )
